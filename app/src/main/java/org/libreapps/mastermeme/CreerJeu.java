7c6dcd9ab453025ee9c0b2145b89a0b29aa95be0
Antoine <antoinevalat01@gmail.com>
2024-05-16 10:39:38 +0200

    voici les changements attribués au projet.

--- app/src/main/java/org/libreapps/mastermeme/CreerJeu.java
+++ app/src/main/java/org/libreapps/mastermeme/CreerJeu.java
@@ -4,12 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+
 
 public class CreerJeu extends AppCompatActivity {
 
@@ -34,6 +38,34 @@ public class CreerJeu extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        // Identifier le groupe radio et les boutons radio
+        RadioGroup radioGroupRounds = findViewById(R.id.radioGroupRounds);
+        RadioButton radioButtonRd6 = findViewById(R.id.radioButtonRd6);
+        RadioButton radioButtonRd12 = findViewById(R.id.radioButtonRd12);
+        RadioButton radioButtonRd18 = findViewById(R.id.radioButtonRd18);
+
+
+        /*
+        // Ajouter un écouteur pour gérer la sélection des boutons radio
+        radioGroupRounds.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                // Gérer les actions en fonction du bouton radio sélectionné
+                switch (checkedId) {
+                    case R.id.radioButtonRd6:
+                        // Actions pour 6 Rounds sélectionnés
+                        break;
+                    case R.id.radioButtonRd12:
+                        // Actions pour 12 Rounds sélectionnés
+                        break;
+                    case R.id.radioButtonRd18:
+                        // Actions pour 18 Rounds sélectionnés
+                        break;
+                }
+            }
+        });
+        */
 
     }
 }
