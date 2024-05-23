03b542aa9411d97626279592d57fd8f465330245
Antoine <antoinevalat01@gmail.com>
2024-05-23 11:59:02 +0200

    voici les changements attribués au projet.

--- app/src/main/java/org/libreapps/mastermeme/APropos.java
+++ app/src/main/java/org/libreapps/mastermeme/APropos.java
@@ -5,23 +5,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 
 public class APropos extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_apropos);
+
+        // Configuration de la gestion des insets
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+            return WindowInsetsCompat.CONSUMED;
         });
 
         Button buttonRetour = findViewById(R.id.button_retour);
@@ -35,5 +36,9 @@ public class APropos extends AppCompatActivity {
             }
         });
 
+        // Activer le mode Edge-to-Edge
+        WindowInsetsControllerCompat windowInsetsController = new WindowInsetsControllerCompat(getWindow(), getWindow().getDecorView());
+        windowInsetsController.setSystemBarsBehavior(WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars());
     }
 }
